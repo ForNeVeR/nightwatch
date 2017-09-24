4852adab4f30405a3866ae8dca28070c87ae97c3
Friedrich von Never <friedrich@fornever.me>
2017-09-24 20:16:15 +0700

    #3: fix FileSystem tests

--- Nightwatch.Tests/FileSystem.fs
+++ Nightwatch.Tests/FileSystem.fs
@@ -16,10 +16,13 @@ type private MockFileSystem =
         member this.Dispose() =
             Directory.Delete(this.root, true)
 
-let private createFileSystemEntry (path : string) =
-    if path.EndsWith "/"
+let rec private createFileSystemEntry (path : string) =
+    if path.EndsWith "/" || path.EndsWith(Path.DirectorySeparatorChar)
     then ignore <| Directory.CreateDirectory path
     else
+        let parent = Path.GetDirectoryName(path) + "/"
+        if not (Directory.Exists parent)
+        then createFileSystemEntry parent
         use x = File.Create(path)
         ()
 
@@ -30,21 +33,23 @@ let private createFileSystem paths =
     paths |> Seq.map (fun p -> Path.Combine(root, p)) |> Seq.iter createFileSystemEntry
     { root = root }
 
+[<Fact>]
 let ``getFilesRecursively should return only the files corresponding to mask`` () =
     let fileList = [| "dir/"; "dir/file.txt"; "dir/file.yml"
-                      "dir/subdir"; "dir/subdir/file.txt"; "dir/subdir/file.yml" |]
+                      "dir/subdir/"; "dir/subdir/file.txt"; "dir/subdir/file.yml" |]
     async {
         use dir = createFileSystem fileList
         let! files = fs.getFilesRecursively (Path dir.root) (Mask "*.txt")
         let expected =
             fileList
             |> Seq.filter (fun p -> p.EndsWith ".txt")
-            |> Seq.map Path
+            |> Seq.map (fun p -> Path(Path.Combine(dir.root, p.Replace("/", string Path.DirectorySeparatorChar))))
             |> Seq.sort
             |> Seq.toArray
-        Assert.Equal<Path>(expected, files)
+        Assert.Equal<Path>(expected, files |> Seq.sort)
     } |> Async.StartAsTask
 
+[<Fact>]
 let ``openStream returns the file stream`` () =
     async {
         let path = Path.GetTempFileName()

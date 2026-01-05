cdff7334ee5c58c742f10b6333172ae504fb63ba
Friedrich von Never <friedrich@fornever.me>
2026-01-05 23:12:27 +0100

    CI: generate via Generaptor

---
+++ scripts/github-actions.fsx
@@ -0,0 +1,110 @@
+let licenseHeader = """
+# SPDX-FileCopyrightText: 2024-2026 Friedrich von Never <friedrich@fornever.me>
+#
+# SPDX-License-Identifier: MIT
+
+# This file is auto-generated.""".Trim()
+
+#r "nuget: Generaptor.Library, 1.8.0"
+
+open Generaptor
+open Generaptor.GitHubActions
+open type Generaptor.GitHubActions.Commands
+
+let workflows = [
+
+    let workflow name steps =
+        workflow name [
+            header licenseHeader
+            yield! steps
+        ]
+
+    let dotNetJob id steps =
+        job id [
+            setEnv "DOTNET_CLI_TELEMETRY_OPTOUT" "1"
+            setEnv "DOTNET_NOLOGO" "1"
+            setEnv "NUGET_PACKAGES" "${{ github.workspace }}/.github/nuget-packages"
+
+            step(
+                name = "Check out the sources",
+                usesSpec = Auto "actions/checkout"
+            )
+            step(
+                name = "Set up .NET SDK",
+                usesSpec = Auto "actions/setup-dotnet"
+            )
+            step(
+                name = "Cache NuGet packages",
+                usesSpec = Auto "actions/cache",
+                options = Map.ofList [
+                    "key", "${{ runner.os }}.nuget.${{ hashFiles('**/*.*proj', '**/*.props') }}"
+                    "path", "${{ env.NUGET_PACKAGES }}"
+                ]
+            )
+
+            yield! steps
+        ]
+
+    workflow "main" [
+        name "Main"
+        onPushTo "master"
+        onPullRequestTo "master"
+        onSchedule "0 0 * * 6"
+        onWorkflowDispatch
+
+        dotNetJob "verify-workflows" [
+            runsOn "ubuntu-24.04"
+            step(run = "dotnet fsi ./scripts/github-actions.fsx verify")
+        ]
+
+        dotNetJob "check" [
+            strategy(failFast = false, matrix = [
+                "image", [
+                    "macos-14"
+                    "ubuntu-24.04"
+                    "ubuntu-24.04-arm"
+                    "windows-11-arm"
+                    "windows-2025"
+                ]
+            ])
+            runsOn "${{ matrix.image }}"
+
+            step(
+                name = "Build",
+                run = "dotnet build"
+            )
+            step(
+                name = "Test",
+                run = "dotnet test",
+                timeoutMin = 10
+            )
+        ]
+
+        job "licenses" [
+            runsOn "ubuntu-24.04"
+            step(
+                name = "Check out the sources",
+                usesSpec = Auto "actions/checkout"
+            )
+            step(
+                name = "REUSE license check",
+                usesSpec = Auto "fsfe/reuse-action"
+            )
+        ]
+
+        job "encoding" [
+            runsOn "ubuntu-24.04"
+            step(
+                name = "Check out the sources",
+                usesSpec = Auto "actions/checkout"
+            )
+            step(
+                name = "Verify encoding",
+                shell = "pwsh",
+                run = "Install-Module VerifyEncoding -Repository PSGallery -RequiredVersion 2.2.1 -Force && Test-Encoding"
+            )
+        ]
+    ]
+]
+
+exit <| EntryPoint.Process fsi.CommandLineArgs workflows
